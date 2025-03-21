5bd126c1f9139ec8a66571b36651d51668c7e2d0
josh-deb <joshc@slac.stanford.edu>
2025-03-20 19:00:19 -0700

    added argparse, made df output cleaner

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from threading import Thread
 from ctypes import c_bool
 import logging
 from functools import partial
+import argparse
 
 
 class DataCollector():
@@ -22,7 +23,7 @@ class DataCollector():
         }
         self.run_time = run_time
         self.sample_period_s = sample_period_s
-        self.num_samps = int(run_time * 1.0 / sample_period_s) + 1
+        self.num_samps = int(run_time * 1.0 / sample_period_s)
 
         # Make space for PVs + Timestamp
         self.data = np.zeros((self.num_samps, len(self.pv_dict)+1))
@@ -59,7 +60,6 @@ class DataCollector():
         while (self.do_work.value and 
                (current_time - start_time) < self.run_time):
             loop_start = current_time
-            print(idx)
             self.data[idx, 0] = current_time - start_time
             # iterate through keys
             i = 1
@@ -83,7 +83,20 @@ def sigint_handler(signum, frame, obj):
 
 
 if __name__ == "__main__":
-    dc = DataCollector(60, 0.1)
+    # do argparse
+    parser = argparse.ArgumentParser("PV DataCollector")
+    parser.add_argument("-f", "--file_name", type=str,
+                        help="relative path which to save data")
+    parser.add_argument("-s", "--sample_period_seconds", 
+                        type=float, default=0.1,
+                        help="period between sample collections in seconds")
+    parser.add_argument("-r", "--run_time_seconds",
+                        type=float, default=60,
+                        help="period over which to collect samples in seconds")
+
+    args = parser.parse_args()
+
+    dc = DataCollector(args.run_time_seconds, args.sample_period_seconds)
     dc.start_work()
     signal.signal(signal.SIGINT, partial(sigint_handler, obj=dc))
 
@@ -94,4 +107,6 @@ if __name__ == "__main__":
     # Create dataframe
     df = pd.DataFrame(data=dc.data,
                       columns=dc.colums)
-    df.to_csv("eggs.csv")
+    df.to_csv(path_or_buf=args.file_name,
+              float_format="%.3f",
+              index=False)
